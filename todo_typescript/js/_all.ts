77693cba0e383a69e38b4396c4268df5d979a42e
hiogawa <hi.ogawa.zz@gmail.com>
2015-08-01 05:15:02 +0900

    setup references between tsc files

---
+++ todo_typescript/js/_all.ts
@@ -0,0 +1,4 @@
+/// <reference path='libs/jquery.d.ts' />
+/// <reference path='libs/underscore.d.ts' />
+/// <reference path='libs/backbone.d.ts' />
+/// <reference path='app.ts' />
---
+++ todo_typescript/js/app.js
@@ -0,0 +1,5 @@
+/// <reference path='libs/jquery.d.ts' />
+/// <reference path='libs/underscore.d.ts' />
+/// <reference path='libs/backbone.d.ts' />
+/// <reference path='app.ts' /> 
+/// <reference path='_all.ts' /> 
